4e8b0f7a5efa15bf0c23700f9c246638845c848f
Moe <hamodeh@Moes-MacBook-Pro.local>
2024-07-24 16:31:47 +0300

    Deleting the Storyboard and setting up the initial view controller programmatically

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Moe on 24/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+}
+
