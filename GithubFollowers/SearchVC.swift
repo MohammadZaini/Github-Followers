6fd155ec945fe9243626559ed27ad14fe19b21a3
Moe <hamodeh@Moes-MacBook-Pro.local>
2024-07-24 17:05:49 +0300

    Setting up the navigation foundation

---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Moe on 24/07/2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+}
