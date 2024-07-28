b562594a16b97c7455617ccb03e8a18a0a6c1fee
Moe <hamodeh@Moes-MacBook-Pro.local>
2024-07-28 17:54:13 +0300

    Showing an activity indicator when pulling the next followers list

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -51,14 +51,17 @@ class FollowerListVC: UIViewController {
     
     private func getFollowers(username: String, page: Int) {
         
+        showLoadingView()
+        
         NetworkManager.shared.getFollowers(username: username, page: page) { [weak self] result in
             
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
                 if followers.count > 100 { hasMoreFollowers = false }
-                self.followers.append(contentsOf: followers)                
+                self.followers.append(contentsOf: followers)
                 self.updateData()
                 
             case .failure(let error):
